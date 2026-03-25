47d0ddfc1f4796a63e3b05185fbee14830876650
Aleksa Pajić <115644202+aleksapajic02@users.noreply.github.com>
2026-03-25 17:32:59 +0100

    Update server.go
    Added curly brcket to end func PayoutMoneyToOtherAccount

--- internal/bank/server.go
+++ internal/bank/server.go
@@ -1343,6 +1343,7 @@ func (s *Server) PayoutMoneyToOtherAccount(
 		Status:          "realized",
 		Timestamp:       time.Now().Format("2006-01-02 15:04:05"), //standard layout of Go, don't change this date, it's layout, not hardcode
 	}, nil
+}
 func (s *Server) TransferMoneyBetweenAccounts(
 	ctx context.Context,
 	req *bankpb.TransferRequest,
