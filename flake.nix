e461aa6acd1b86d4a9e856f239239c59391099c2
urosevicvuk <vuk23urosevic@gmail.com>
2026-05-01 11:41:58 +0200

    mostly ready, added more services
    Signed-off-by: urosevicvuk <vuk23urosevic@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {flake-parts, ...}:
@@ -15,8 +17,8 @@
         "aarch64-darwin"
       ];
       imports = [
-        ./nix/devshell.nix
-        ./nix/services.nix
+        ./utils/nix/devshell.nix
+        ./utils/nix/services.nix
       ];
     };
 }
--- utils/nix/services.nix
+++ utils/nix/services.nix
@@ -1,15 +1,23 @@
 {lib, ...}: let
-  services = ["bank"];
+  services = ["bank" "exchange" "gateway" "notification" "user"];
 
   vendorHashes = {
     bank = "sha256-UIufAOnWzZ5vfG/ObL/xlNroZLoLWwvBeNU3fuHS7RY=";
+    exchange = "sha256-q3ntU0dZD0wGrnFlCPUyynVoo8b218CiM2Kas5e52ZQ=";
+    gateway = "sha256-KijZA42AGZhtafPBhkn8v40pSKjdMdEPgyp04bHr/PI=";
+    notification = "sha256-GZ+Iu6fk5SK0RqisO5oa4HLuh4O4ICzHUUojMYbjiec=";
+    user = "sha256-xTwsoLN9mCKwUv4iNL4umJMjEko9yOmg0Odnl21emHk=";
   };
 
+  repoSrc = lib.cleanSource ../..;
+
+  mkGoBuild = pkgs: pkgs.buildGoModule.override {go = pkgs.go_1_25;};
+
   mkBinary = pkgs: name:
-    pkgs.buildGo125Module {
+    (mkGoBuild pkgs) {
       pname = name;
       version = "0.0.0";
-      src = lib.cleanSource ../.;
+      src = repoSrc;
       modRoot = "services/${name}";
       subPackages = ["cmd"];
       vendorHash = vendorHashes.${name};
@@ -23,26 +31,99 @@
       '';
     };
 
-  mkImage = pkgs: name: bin:
-    pkgs.dockerTools.buildLayeredImage {
+  mkTest = pkgs: name:
+    (mkGoBuild pkgs) {
+      pname = "${name}-test";
+      version = "0.0.0";
+      src = repoSrc;
+      modRoot = "services/${name}";
+      subPackages = ["cmd"];
+      vendorHash = vendorHashes.${name};
+      env = {
+        CGO_ENABLED = "1";
+        GOWORK = "off";
+      };
+      doCheck = true;
+      checkPhase = ''
+        runHook preCheck
+        go test -race -count=1 ./...
+        runHook postCheck
+      '';
+    };
+
+  mkLint = pkgs: name: let
+    bin = mkBinary pkgs name;
+  in
+    pkgs.stdenv.mkDerivation {
+      name = "${name}-lint";
+      src = repoSrc;
+      nativeBuildInputs = [pkgs.go_1_25 pkgs.golangci-lint];
+      buildPhase = ''
+        runHook preBuild
+        export HOME=$TMPDIR
+        export GOFLAGS='-mod=vendor'
+        export GOPROXY=off
+        export GOWORK=off
+        export GOCACHE=$TMPDIR/go-cache
+        export GOLANGCI_LINT_CACHE=$TMPDIR/lint-cache
+        chmod -R u+w services/${name}
+        cp -r --no-preserve=mode ${bin.goModules} services/${name}/vendor
+        cd services/${name}
+        golangci-lint run --timeout=5m ./...
+        runHook postBuild
+      '';
+      installPhase = "touch $out";
+    };
+
+  mkImage = n2c: pkgs: name: bin:
+    n2c.buildImage {
       name = "ghcr.io/raf-si-2025/banka-3-backend-${name}";
       tag = "latest";
-      contents = [pkgs.cacert];
-      config.Cmd = ["${bin}/bin/${name}"];
+      copyToRoot = [pkgs.cacert];
+      config = {
+        Entrypoint = ["${bin}/bin/${name}"];
+      };
     };
+
+  mkPerSvc = suffix: f:
+    lib.listToAttrs (map (name: {
+        name =
+          if suffix == ""
+          then name
+          else "${name}-${suffix}";
+        value = f name;
+      })
+      services);
 in {
-  perSystem = {pkgs, ...}: let
-    binaries = lib.genAttrs services (mkBinary pkgs);
-    images =
-      lib.mapAttrs'
-      (name: bin: lib.nameValuePair "${name}-image" (mkImage pkgs name bin))
-      binaries;
-    buildChecks =
-      lib.mapAttrs'
-      (name: bin: lib.nameValuePair "${name}-build" bin)
-      binaries;
+  perSystem = {
+    pkgs,
+    inputs',
+    ...
+  }: let
+    n2c = inputs'.nix2container.packages.nix2container;
+
+    binaries = mkPerSvc "" (mkBinary pkgs);
+    images = mkPerSvc "image" (name: mkImage n2c pkgs name binaries.${name});
+
+    buildChecks = mkPerSvc "build" (name: binaries.${name});
+    testChecks = mkPerSvc "test" (mkTest pkgs);
+    lintChecks = mkPerSvc "lint" (mkLint pkgs);
+
+    fmtCheck = pkgs.runCommandLocal "fmt-check" {
+      nativeBuildInputs = [pkgs.go_1_25];
+      src = repoSrc;
+    } ''
+      cd $src
+      bad=$(gofmt -s -l services pkg)
+      if [ -n "$bad" ]; then
+        echo "Files need gofmt:"
+        echo "$bad"
+        exit 1
+      fi
+      touch $out
+    '';
   in {
     packages = binaries // images;
-    checks = buildChecks;
+    checks = buildChecks // testChecks // lintChecks // {fmt = fmtCheck;};
   };
 }
